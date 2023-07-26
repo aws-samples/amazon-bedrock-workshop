b667c70fdce7cf04d8fb1331372b1a06944ca090
Alex Thewsey <thewsey@amazon.com>
2023-07-26 08:04:42 +0000

    fix(deps): download-dependencies.sh in SMStudio
    chmod download-dependencies.sh to (hopefully) allow executing the
    script by default after git clone. Add a branch to extract the SDK
    file via Python in case the `unzip` CLI utility is not installed -
    which is the case in SageMaker Studio.

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -6,5 +6,11 @@ cd ./dependencies && \
 echo "Downloading dependencies"
 curl -sS https://preview.documentation.bedrock.aws.dev/Documentation/SDK/bedrock-python-sdk.zip > sdk.zip && \
 echo "Unpacking dependencies"
-unzip sdk.zip && \
-rm sdk.zip
+# (SageMaker Studio system terminals don't have `unzip` utility installed)
+if command -v unzip &> /dev/null
+then
+    unzip sdk.zip && rm sdk.zip
+else
+    echo "'unzip' command not found: Trying to unzip via Python"
+    python -m zipfile -e sdk.zip . && rm sdk.zip
+fi

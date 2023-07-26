47cc886800c0fdf64d290619467068c57433e45d
Alex Thewsey <thewsey@amazon.com>
2023-07-26 08:08:45 +0000

    feat(deps): Echo when done

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -9,8 +9,8 @@ echo "Unpacking dependencies"
 # (SageMaker Studio system terminals don't have `unzip` utility installed)
 if command -v unzip &> /dev/null
 then
-    unzip sdk.zip && rm sdk.zip
+    unzip sdk.zip && rm sdk.zip && echo "Done"
 else
     echo "'unzip' command not found: Trying to unzip via Python"
-    python -m zipfile -e sdk.zip . && rm sdk.zip
+    python -m zipfile -e sdk.zip . && rm sdk.zip && echo "Done"
 fi

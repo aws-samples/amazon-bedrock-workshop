eb368f82aba69d47da728831c96e4ebe4bf73963
subshrey <subshrey@amazon.com>
2026-04-22 15:58:39 -0400

    Add configurable basePath for SageMaker Studio proxy

--- tutor/next.config.ts
+++ tutor/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const studioBasePath = process.env.NEXT_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@copilotkit/runtime"],
+  basePath: studioBasePath,
+  assetPrefix: studioBasePath ? studioBasePath + "/" : "",
 };
 
 export default nextConfig;
